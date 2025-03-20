22b8b09bfd99b1c4601a0fec7d4442f8baee9565
mahee <mfernando@controlunion.com>
2025-03-20 11:15:16 +0530

    Add auth route

---
+++ src/routes/admin/auth.route.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { signup ,signin } from '../../controllers/admin/auth.controller';
+
+
+const router = express.Router();
+
+
+router.post('/signup',signup)
+router.post('/signin',signin)
+
+
+
+export default router;
