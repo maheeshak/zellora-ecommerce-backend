767a67722462109185b33096d19cfdf8ac07b952
mahee <mfernando@controlunion.com>
2025-03-20 11:14:09 +0530

    Add color model

---
+++ src/models/color.model.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+
+const colorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    code: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
+
+
+const Color = mongoose.model("Color", colorSchema);
+
+export default Color;
